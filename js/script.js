f77b59e027df0d0646eca18262c8b65a91402c2d
JaminTech <whodid.info@gmail.com>
2023-07-07 23:12:38 -0500

    Portfolio-website

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function RunTypingEffect()
+{
+    const text = "I'm Ben Frazier";
+    const typingElement = document.getElementById('typing-text');
+    const typingDelay = 100;
+    
+    typeText(text, typingElement, typingDelay);
+}
+
+function typeText (text, typingElement, typingDelay) 
+{
+    for(let i=0; i<text.length; i++) 
+    {
+        setTimeout(() => {
+            typingElement.textContent += text.charAt(i);
+        }, typingDelay * i);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', RunTypingEffect);
